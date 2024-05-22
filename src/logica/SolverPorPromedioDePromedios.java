b0a9d40f3c304924b45ff5ae036eb365930b0c84
SantiagoZarate <santiago2001zarate@gmail.com>
2024-05-22 12:42:56 -0300

    test: añade archivo de test para la nueva heuristica y stress test

---
+++ src/logica/SolverPorPromedioDePromedios.java
@@ -0,0 +1,57 @@
+package logica;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SolverPorPromedioDePromedios {
+
+	@Test
+	@Ignore
+	public void SolverPorVerticeMasPesadoTest_grafo4() {
+		Grafo grafo = new InstanciasGrafo().grafo_4();
+
+		Solucion solucion = new Solver(grafo, Comparadores.porSumaPromedioVecindarioVecinos).resolver();
+
+		assertEquals(25, solucion.peso());
+		assertEquals(3, solucion.cantidadVertices());
+	}
+
+	@Test
+	@Ignore
+	public void StressTest() {
+		int[] CANTIDAD_VERTICES = { 1000, 10000, 20000, 80000 };
+
+		for (Integer cant : CANTIDAD_VERTICES) {
+			Grafo grafo = generarGrafoCompleto(cant);
+
+			System.out.println("Grafo con " + cant + " vertices.");
+
+			long startTime = System.nanoTime();
+			Solucion solucion = new Solver(grafo, Comparadores.porSumaPromedioVecindarioVecinos).resolver();
+			long endTime = System.nanoTime();
+
+			long executionTime = (endTime - startTime) / 1000000;
+			System.out.println("Execution time: " + executionTime + "ms.\n");
+		}
+	}
+
+	private Grafo generarGrafoCompleto(int cantidadVertices) {
+		Grafo grafo = new Grafo();
+
+		for (int i = 0; i < cantidadVertices; i++) {
+			grafo.agregarVertice(10);
+		}
+
+		for (int i = 0; i < grafo.tamano(); i++) {
+			for (int j = i + 1; i < grafo.tamano(); i++) {
+				if (i != j) {
+					grafo.agregarArista(i, j);
+				}
+			}
+		}
+		return grafo;
+	}
+
+}
