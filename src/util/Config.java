7ffa3ab9950c301420ea375e4432712d38a2bf7a
SantiagoZarate <santiago2001zarate@gmail.com>
2024-05-24 11:12:52 -0300

    styles: cambia estilos de los nodos y aristas

--- src/util/Config.java
+++ src/util/Config.java
@@ -6,19 +6,39 @@ public class Config {
 	public static Color COLOR_PANEL = new Color(200, 241, 254);
 	public static Color COLOR_BOTON = new Color(23, 90, 115);
 	public static Color COLOR_BOTON_SALIR = new Color(179, 0, 0);
-	
+
 	public static String ESTILOS_GRAPHSTREAM = """
 			graph {
 				fill-color: rgb(200, 241, 254);
 			}
-			
+
 			node{
 				text-color: #111;
 				text-size: 14px;
-				text-style: bold;
+				text-style: bold-italic;
 				size: 50px, 50px;
-				fill-color: rgb(106, 226, 246);
-				text-mode: normal;
+				fill-mode:gradient-diagonal1 ;
+				fill-color: rgb(106, 226, 246), rgb(156, 255, 255);
+				
+				text-offset: 0px, 40px;
+				
+				text-background-color:  rgb(86, 206, 226);
+				text-background-mode: rounded-box;
+				text-padding: 8px;
+				
+				stroke-color: rgb(86, 206, 226);
+				stroke-width: 3px;
+				stroke-mode: plain;
+			}
+			
+			node:clicked {
+				fill-color: rgb(66, 186, 206);
+			}
+			
+			edge{
+				shape:cubic-curve;
+				fill-color: rgb(66, 186, 206);
+				size: 2px;
 			}
 			""";
 }
