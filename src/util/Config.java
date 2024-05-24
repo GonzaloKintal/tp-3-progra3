b37404cb2bdc8b2766df8682b7e6f75a44fc80aa
SantiagoZarate <santiago2001zarate@gmail.com>
2024-05-24 10:29:35 -0300

    chore: reescritura de los estilos css mas entendible

--- src/util/Config.java
+++ src/util/Config.java
@@ -7,13 +7,18 @@ public class Config {
 	public static Color COLOR_BOTON = new Color(23, 90, 115);
 	public static Color COLOR_BOTON_SALIR = new Color(179, 0, 0);
 	
-	public static String ESTILOS_GRAPHSTREAM = "graph { fill-color: rgb(200, 241, 254);}" + 
-    		"node{\n" +
-    		"    text-color: #111;\n" +
-    		"    text-size: 14px;\n" +
-    		"    text-style: bold;\n" +
-            "    size: 50px, 50px;\n" +
-            "    fill-color: rgb(106, 226, 246);\n" +
-            "    text-mode: normal; \n" +
-            "}";
+	public static String ESTILOS_GRAPHSTREAM = """
+			graph {
+				fill-color: rgb(200, 241, 254);
+			}
+			
+			node{
+				text-color: #111;
+				text-size: 14px;
+				text-style: bold;
+				size: 50px, 50px;
+				fill-color: rgb(106, 226, 246);
+				text-mode: normal;
+			}
+			""";
 }
